c485614f18febbbc10d8fba101e13a39b7fcdfb1
Anurag Singh <anurag0577@gmail.com>
2025-01-19 22:38:34 +0530

    First commit.

---
+++ Arrays/Easy/helloWorld.java
@@ -0,0 +1,10 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        printMessage("Hello, World!");
+        printMessage("Welcome to Java Programming!");
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
