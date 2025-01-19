46500c3720a9751307671dddda65a76c431849ad
Anurag Singh <anurag0577@gmail.com>
2025-01-20 00:30:18 +0530

    second change.

--- Arrays/Easy/helloWorld.java
+++ Arrays/Easy/helloWorld.java
@@ -2,6 +2,7 @@ public class helloWorld {
     public static void main(String[] args) {
         printMessage("Hello, World!");
         printMessage("Welcome to Java Programming!");
+        System.out.println("hello world!");
     }
 
     public static void printMessage(String message) {
